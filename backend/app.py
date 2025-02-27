6dbd61850d84b4e5bfe933459e1f7b1a18aff08e
Silas Teague <silasteague@gmail.com>
2025-02-26 22:30:30 -0500

    Initialize database and bcrypt

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,20 @@
+import os
 from flask import Flask, request, jsonify
+from backend.models import db, bcrypt
 
 app = Flask(__name__)
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../database"))
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(BASE_DIR, "users.db")}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'your_secret_key'
+
+# Initialize database and bcrypt
+db.init_app(app)
+bcrypt.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/", methods=["GET"])
 def home():
