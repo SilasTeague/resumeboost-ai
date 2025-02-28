58e7b73a82d1e0be968761f47a07c1d05c7cf35c
Silas Teague <silasteague@gmail.com>
2025-02-28 18:24:52 -0500

    Add set_password() and check_password() methods to User model

--- backend/models.py
+++ backend/models.py
@@ -10,3 +10,8 @@ class User(db.Model):
     password_hash = db.Column(db.String(128), nullable=False)
     security_question = db.Column(db.String(200), nullable=False)
     security_answer = db.Column(db.String(200), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
