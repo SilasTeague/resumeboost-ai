d01b7a01d97b6eb6013f7fd599d5eeb6296be3bc
Silas Teague <silasteague@gmail.com>
2025-03-01 23:48:32 -0500

    Configure JWT authentication in Flask app

--- backend/app.py
+++ backend/app.py
@@ -2,17 +2,20 @@ import os
 from flask import Flask, request, jsonify
 from backend.models import db, bcrypt
 from backend.resume_routes import resume_bp
+from flask_jwt_extended import JWTMANAGER
 
 app = Flask(__name__)
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../database"))
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(BASE_DIR, "users.db")}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SECRET_KEY'] = 'secret_key'
+app.config['JWT_SECRET_KEY'] = 'jwt_secret_key'
 
 # Initialize database and bcrypt
 db.init_app(app)
 bcrypt.init_app(app)
+jwt = JWTMANAGER
 
 app.register_blueprint(resume_bp, url_prefix="/resume")
 
